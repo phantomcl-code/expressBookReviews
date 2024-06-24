71b95aa9ddd7f6c588231c95c967d0e49cd1bacf
rafael cruz lillo <rafaelcruz@rafaels-MacBook-Pro.local>
2024-06-24 18:55:11 -0400

    1,2,3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,3 +1,5 @@
+const isNil = require('lodash/isNil');
+
 const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
@@ -13,19 +15,39 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json(JSON.stringify(books));
+  return res.status(200).json(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN = req.params.isbn;
+
+  res.send(books[ISBN])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const response = [];
+
+    if(isNil(author)) {
+        res.status(400).send("author is null or undefined");
+    }
+
+    for (let id in books) {
+        if (books[id].author.toLowerCase() === author.toLowerCase()) {
+            response.push(books[id]);
+        }
+    }
+
+    if (!isNil(response) && response.length > 0) {
+        res.send(response);
+    } else {
+        res.status(404).send("Author not found");
+    }
+
 });
 
 // Get all books based on title
